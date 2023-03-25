48eb04855f216e33c8b6c14dc79d6ebe2798c1c5
Krish1903 <98330531+Krish1903@users.noreply.github.com>
2023-03-25 00:01:03 -0400

    Added UserLoginView, login API complete (Dummy JSON data: passed)

--- MyApp/userAPI/urls.py
+++ MyApp/userAPI/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import TestView, UsersView
+from .views import TestView, UsersView, UserLoginView
 
 urlpatterns = [
     path("test", TestView.as_view()),
-    path("create-user/", UsersView.as_view())
+    path("create-user/", UsersView.as_view()),
+    path("get-user", UserLoginView.as_view()),
+    path('login-user/', UserLoginView.as_view()),
 ]
 
 
--- MyApp/userAPI/views.py
+++ MyApp/userAPI/views.py
@@ -4,6 +4,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .serializers import UserSerializer
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
 # Create your views here.
 
 
@@ -23,6 +27,7 @@ class TestView(APIView):
 
 
 class UsersView(APIView):
+
     def post(self, request, format=None):
         print("Creating a user")
 
@@ -36,11 +41,32 @@ class UsersView(APIView):
 
         return Response({"msg": "ERR in API"}, status=400)
 
-    def get():
-        ...
 
-    def put():
-        ...
+class UserLoginView(APIView):
+    # Convert a user token into user data
+    def get(self, request, format=None):
 
-    def repeat():
-        ...
+        if request.user.is_authenticated == False or request.user.is_active == False:
+            return Response("Invalid Credentials", status=403)
+
+        user = UserSerializer(request.user)
+        return Response(user.data, status=200)
+
+    def post(self, request, format=None):
+        print("Login Class")
+
+        user_obj = User.objects.filter(email=request.data['username']).first(
+        ) or User.objects.filter(username=request.data['username']).first()
+
+        if user_obj is not None:
+            credentials = {
+                'username': user_obj.username,
+                'password': request.data['password']
+            }
+            user = authenticate(**credentials)
+
+            if user and user.is_active:
+                user_serializer = UserSerializer(user)
+                return Response(user_serializer.data, status=200)
+
+        return Response("Invalid Credentials", status=403)
