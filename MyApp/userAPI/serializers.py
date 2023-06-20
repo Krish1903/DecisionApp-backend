2ea0f84903d74829272c00405069e628f0d8b822
Krish1903 <98330531+Krish1903@users.noreply.github.com>
2023-06-20 17:02:04 -0400

    added id to user serializer

--- MyApp/userAPI/serializers.py
+++ MyApp/userAPI/serializers.py
@@ -34,6 +34,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            'id',
             'username',
             'password',
             'email',
