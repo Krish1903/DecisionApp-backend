63dc28afc48c34105ec241d34ff54ffb561a947e
Krish1903 <98330531+Krish1903@users.noreply.github.com>
2023-07-24 17:53:45 -0400

    Configured for Postgres on Heroku

--- MyApp/MyApp/settings.py
+++ MyApp/MyApp/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "MyApp.wsgi.application"
 
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(default='postgres://localhost')
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
