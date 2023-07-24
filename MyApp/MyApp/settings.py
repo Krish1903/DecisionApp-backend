1c2fafed52aab8a2250f5176e73194b2fa3729cc
Krish1903 <98330531+Krish1903@users.noreply.github.com>
2023-07-24 17:36:17 -0400

    updated settings

--- MyApp/MyApp/settings.py
+++ MyApp/MyApp/settings.py
@@ -86,10 +86,8 @@ WSGI_APPLICATION = "MyApp.wsgi.application"
 
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        }
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
