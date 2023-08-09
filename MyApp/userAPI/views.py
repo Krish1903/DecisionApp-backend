4b966c0217ba510bf4589ed82e089dc24b40425e
Krish1903 <98330531+Krish1903@users.noreply.github.com>
2023-08-09 03:32:44 -0400

    Changed search view

--- MyApp/userAPI/views.py
+++ MyApp/userAPI/views.py
@@ -568,34 +568,13 @@ class VotedPollsView(APIView):
 
         return Response(serializer.data, status=200)
 
+
 class UserSearchView(APIView):
     def get(self, request, search_string=None, *args, **kwargs):
         current_user = request.user
 
-        # Check if the user is authenticated
         if not current_user.is_authenticated:
-            base_query = User.objects.exclude(id=current_user.id)
-
-            if search_string:
-                users = base_query.filter(
-                    Q(username__icontains=search_string) |
-                    Q(first_name__icontains=search_string) |
-                    Q(last_name__icontains=search_string)
-                )
-                
-                followed_users = current_user.useraccount.following.all().values_list('user__id', flat=True)
-                users = users.exclude(id__in=followed_users)
-
-                if not users.exists() and not search_string.strip() == "":
-                    users = []
-                elif not users.exists() and search_string.strip() == "":
-                    users = base_query.order_by('first_name')
-            else:
-                followed_users = current_user.useraccount.following.all().values_list('user__id', flat=True)
-                users = base_query.exclude(id__in=followed_users).order_by('first_name')
-
-            user_serializer = UserSerializer(users, many=True)
-            return Response(user_serializer.data, status=200)
+            return Response([], status=200)  
 
         base_query = User.objects.exclude(id=current_user.id)
 
@@ -606,16 +585,20 @@ class UserSearchView(APIView):
                 Q(last_name__icontains=search_string)
             )
             
-            followed_users = current_user.useraccount.following.all().values_list('user__id', flat=True)
-            users = users.exclude(id__in=followed_users)
+            try:
+                followed_users = current_user.useraccount.following.all().values_list('user__id', flat=True)
+                users = users.exclude(id__in=followed_users)
+            except AttributeError:
+                pass
 
-            if not users.exists() and not search_string.strip() == "":
-                users = []
-            elif not users.exists() and search_string.strip() == "":
+            if not users.exists():
                 users = base_query.order_by('first_name')
         else:
-            followed_users = current_user.useraccount.following.all().values_list('user__id', flat=True)
-            users = base_query.exclude(id__in=followed_users).order_by('first_name')
+            try:
+                followed_users = current_user.useraccount.following.all().values_list('user__id', flat=True)
+                users = base_query.exclude(id__in=followed_users).order_by('first_name')
+            except AttributeError:
+                users = base_query.order_by('first_name')
 
         user_serializer = UserSerializer(users, many=True)
         return Response(user_serializer.data, status=200)
