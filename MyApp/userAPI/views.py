78c0906cfe487a427711b43284bdfb2ffd9e6715
Krish1903 <98330531+Krish1903@users.noreply.github.com>
2023-08-09 03:27:32 -0400

    Changed search view

--- MyApp/userAPI/views.py
+++ MyApp/userAPI/views.py
@@ -574,7 +574,28 @@ class UserSearchView(APIView):
 
         # Check if the user is authenticated
         if not current_user.is_authenticated:
-            return Response({"detail": "User is not authenticated."}, status=200)
+            base_query = User.objects.exclude(id=current_user.id)
+
+            if search_string:
+                users = base_query.filter(
+                    Q(username__icontains=search_string) |
+                    Q(first_name__icontains=search_string) |
+                    Q(last_name__icontains=search_string)
+                )
+                
+                followed_users = current_user.useraccount.following.all().values_list('user__id', flat=True)
+                users = users.exclude(id__in=followed_users)
+
+                if not users.exists() and not search_string.strip() == "":
+                    users = []
+                elif not users.exists() and search_string.strip() == "":
+                    users = base_query.order_by('first_name')
+            else:
+                followed_users = current_user.useraccount.following.all().values_list('user__id', flat=True)
+                users = base_query.exclude(id__in=followed_users).order_by('first_name')
+
+            user_serializer = UserSerializer(users, many=True)
+            return Response(user_serializer.data, status=200)
 
         base_query = User.objects.exclude(id=current_user.id)
 
