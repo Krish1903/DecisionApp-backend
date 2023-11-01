6d4ca8283ab048f51095d0e5eaf13940f21b7174
Krish1903 <98330531+Krish1903@users.noreply.github.com>
2023-11-01 00:50:40 -0400

    changed search values for blocked users

--- MyApp/userAPI/views.py
+++ MyApp/userAPI/views.py
@@ -597,7 +597,7 @@ class UserSearchView(APIView):
             followed_users = current_user.useraccount.following.all().values_list('user__id', flat=True)
 
             blocked_users = list(current_user.useraccount.blocked_users.all().values_list('id', flat=True))
-            blocked_by_users = User.objects.filter(useraccount__blocked_users__in=[current_user]).values_list('id', flat=True)
+            blocked_by_users = User.objects.filter(useraccount__blocked_users=current_user.useraccount).values_list('id', flat=True)
             blocked_users = set(blocked_users).union(blocked_by_users)
 
             base_query = base_query.exclude(id__in=followed_users).exclude(id__in=blocked_users)
