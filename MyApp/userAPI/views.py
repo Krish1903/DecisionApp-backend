0f54b859075868b796bb4a48c4a55c893aaf942d
Krish1903 <98330531+Krish1903@users.noreply.github.com>
2023-11-01 17:54:26 -0400

    Fixed instance for blocked users in following

--- MyApp/userAPI/views.py
+++ MyApp/userAPI/views.py
@@ -489,7 +489,7 @@ class GetFollowing(APIView):
             user_account = user.useraccount
 
             blocked_users = user_account.blocked_users.all()
-            users_blocking = UserAccount.objects.filter(blocked_users=user_account)
+            users_blocking = User.objects.filter(useraccount__blocked_users=user_account)
 
             following = user_account.following.exclude(
                 Q(user__in=blocked_users) | Q(useraccount__in=users_blocking)
